3df5ace52e098d6d331fd1eac98e94a9f3a257ae
jade-nathan <nroche@scene.health>
2023-11-20 17:25:16 -0800

    finished basic implementation and unit tests

--- src/problem1/JSONParser.java
+++ src/problem1/JSONParser.java
@@ -3,10 +3,12 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @FunctionalInterface
-interface parsing {
-    boolean parseFunction();
+interface parseFunction {
+    boolean parse();
 }
 
 @FunctionalInterface 
@@ -19,115 +21,298 @@ public class JSONParser {
     private String json;
     private Map<String, Object> output;
     private ArrayList<Object> outputItems;
+    private parseFunction[] valParsers;
+    
+    public static Map<String,Object> parse(String json){
+    	JSONParser parser = new JSONParser(json);
+    	try {
+    		return parser.execute();
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		return null;
+    	}
+    }
+    
+    public enum nest{
+    	BEGINARRAY,
+    	ENDARRAY,
+    	BEGINOBJECT,
+    	ENDOBJECT,
+    }
+    public class ParsedNumber {
+    	public boolean isNegative;
+    	public boolean isExpNegative;
+    	public String intComp;
+    	public String frac;
+    	public String exp;
+    	
+    	ParsedNumber(){
+    		this.isNegative = false;
+    		this.isExpNegative = false;
+    		this.intComp = "";
+    		this.frac = "";
+    		this.exp = "";
+    	}
+    }
 
     public JSONParser(String json){
         this.pos  = 0;
         this.json = json;
         this.output = new HashMap<String,Object>();
         this.outputItems = new ArrayList<Object>();
+        this.valParsers = new parseFunction[7];
+        this.valParsers[0] = () -> parseString();
+        this.valParsers[1] = () -> parseNumber();
+        this.valParsers[2] = () -> parseLiteral("true", true);
+        this.valParsers[3] = () -> parseLiteral("false", true);
+        this.valParsers[4] = () -> parseLiteral("null", true);
+//    	this.valParsers[5] = () -> parseObject();
+//    	this.valParsers[6] = () -> parseArray();
     }
 
-    public Map<String,Object> parse() throws Exception {
-    	if (!parseObject()) { //do we successfully parse an object?
-
+    public Map<String,Object> execute() throws Exception {
+    	//reset variables.. this is not guaranteed, but it helps if people try to use parser twice
+    	pos = 0;
+    	this.output = new HashMap<String,Object>();
+    	this.outputItems = new ArrayList<Object>();
+    	if (parseObject()) { //do we successfully parse an object?
     		//pack values into map form
     		Iterator<Object> iter = outputItems.iterator();
     		while (iter.hasNext()) {
     			//note: a correct parsing should have even num values correctly formatted
     			output.put((String)iter.next(), iter.next());  
     		}
-    	}
-    	else {
-    		if (!parseArray()) {//do we successfully parse an array?
+    	} else {
+    		if (parseArray()) {//do we successfully parse an array?
     			//a JSON Array will simply be a dictionary with one entry: ("" : ArrayList)
+    			
     			output.put("", outputItems);
-    		}
-    		else
+    		} else {
     			//this is the only failure mode that the parser has
     			//the next addition to this code might be more descriptive failure modes
     			throw new Exception("JSON Format not detected.");
+    		}
     	}
-    	
-    	
         return output;
     }
 
-    public boolean orCombinator(parsing[] parsers) {
-    	ArrayList<Object> saveState = (ArrayList) outputItems.clone();
-    	for (int i = 0; i < parsers.length; i++) {
-	    	if (parsers[i].parseFunction())
-	    		return true;
-	    	outputItems = saveState;
+    public Map<String,Object> addObjectItems(Iterator<Object> iter) {
+    	HashMap<String,Object> objectMap = new HashMap<String,Object>();
+		while (iter.hasNext()) {
+			Object first  = iter.next();
+			if (first instanceof nest) { //if we find a nest object at an odd interval, this means end the nesting
+				return objectMap;
+			}
+			String key = (String) first;
+			Object item = iter.next();
+			if (item instanceof nest ) {
+				switch ((nest)item) {
+					case BEGINARRAY: objectMap.put(key, addArrayItems(iter));
+						 break;
+					case BEGINOBJECT: objectMap.put(key, addObjectItems(iter));
+						break;
+					default: objectMap.put(key, item);
+				}
+						
+			} else {
+				objectMap.put(key, item);
+			}
+		}
+		return objectMap;
+	}
+
+    
+    public ArrayList<Object> addArrayItems(Iterator<Object> iter) {
+    	ArrayList<Object> array = new ArrayList<Object>();
+    	while (iter.hasNext()) {
+    		Object item = iter.next();
+    		if (item instanceof nest) {
+    			switch ((nest)item) {
+					case BEGINARRAY: array.add(addArrayItems(iter));
+						 break;
+					case BEGINOBJECT: array.add(addObjectItems(iter));
+						break;
+					case ENDARRAY: return array;
+					default: array.add(item);
+    			}
+    		} else {
+    			array.add(item);
+    		}
     	}
-    	return false;
+    	return array;
     }
+   
+    
     
     public boolean parseObject(){
-    	boolean isObject = checkChar('{');
+    	if (!checkChar('{')) 
+    		return false;
+    	parseWhitespace();
     	while(pos < json.length() && currentChar() != '}') {
+			if (!parseString())
+	    		return false;
 			parseWhitespace();
-			isObject = isObject && parseValue();
-			parseWhitespace();
+			if (!checkChar(':'))
+	    		return false;
+			if (!parseValue())
+	    		return false;
+			if(!checkChar(','))
+				break;
     	}
-    	isObject = isObject && checkChar('}');
-    	return isObject;
+    	if (!checkChar('}'))
+    		return false;
+    	return true;
     }
 
     public boolean parseArray(){
     	boolean isArray = checkChar('[');
-    	isArray = isArray && parseWhitespace();
+    	parseWhitespace();
+    	while(pos < json.length() && currentChar() != ']') {
+			if (!parseValue())
+				return false;
+			if(!checkChar(','))
+				break;
+    	}
     	isArray = isArray && checkChar(']');
         return isArray;
     }
 
     
     public boolean parseValue(){
-        return false;
+    	parseWhitespace();
+    	boolean succ = orCombinator(valParsers);
+    	parseWhitespace();
+        return succ;
+    }
+    
+    public boolean orCombinator(parseFunction[] parsers) {
+    	ArrayList<Object> saveState = (ArrayList) outputItems.clone();
+    	for (int i = 0; i < parsers.length; i++) {
+	    	if (parsers[i].parse())
+	    		return true;
+	    	outputItems = saveState;
+    	}
+    	return false;
     }
 
     public boolean parseNumber(){
-        return false;
+    	ParsedNumber num = new ParsedNumber();
+    	//check initial characters
+    	num.isNegative = checkChar('-');
+    	if (checkChar('0')) {
+    		if (Character.isDigit(currentChar()))
+    			return false;
+    	} else {
+    		//grab optional integer portion
+    		if (Character.isDigit(currentChar())) {
+    			num.intComp = parseDigits();
+    		}
+    		else
+    			return false;
+    	}
+    	//grab optional fractional portion
+    	if(checkChar('.')) {
+    		num.frac = parseDigits();
+    		if (num.frac.equals("")){
+    			return false;
+    		}
+    	}
+    	
+    	//grab optional exponent portion
+    	if(checkChar('E') || checkChar('e')) {
+    		if (checkChar('-'))
+    			num.isExpNegative = true;
+    		else
+    			checkChar('+');
+    		num.frac = parseDigits();
+    		if (num.frac.equals("")){
+    			return false;
+    		}
+    	}
+    	this.outputItems.add(num);
+        return true;
+    }
+    
+    public String parseDigits() {
+    	String digits = "";
+    	while(!positionInvalid() && Character.isDigit(currentChar())) {
+    		digits = digits + currentChar();
+    		pos++;
+    	}
+    	return digits;
     }
     
     public boolean parseString(){
-        if (checkChar('"'))
+        if (!checkChar('"')) {
             return false;
+        }
         int start = pos;
-        while (!checkChar('"')){}
+        while (!checkChar('"')) {
+        	pos++;
+        	if (positionInvalid()) {
+        		return false;
+        	}
+        }
         int end = pos;
-        outputItems.add(json.substring(start, end));
+        outputItems.add(json.substring(start, end-1));
         return true;
     }
     
     public boolean parseWhitespace(){
-    	while(pos < json.length() && Character.isWhitespace(currentChar())) {
+    	while(!positionInvalid() && Character.isWhitespace(currentChar())) {
             pos++;
         }
         return true;
     }
     
     public boolean parseLiteral(String lit) {
+    	return parseLiteral(lit, false);
+    }
+    
+    public boolean parseLiteral(String lit, boolean storeItem) {
     	for (int i = 0; i < lit.length(); i++) {
     		if (!checkChar(lit.charAt(i))) {
     			return false;
     		}
     	}
+    	if(storeItem) {
+    		outputItems.add(convertLiteral(lit));
+    	}
     	return true;
+    }
+    
+    public Object convertLiteral(String lit) {
+    	if (lit.equals("null"))
+    		return null;
+    	if (lit.equals("true"))
+    		return true;
+    	if (lit.equals("false"))
+    		return false;
+    	return lit;
     }
 
     public char currentChar(){
+    	if (positionInvalid()) {
+    		return ' ';
+    	}
         return json.charAt(pos);
+    }
+    
+    public boolean positionInvalid() {
+    	return pos >= json.length();
     }
     
     public boolean checkChar(char check) {
         return checkChar((c) -> c == check);
     }
     public boolean checkChar(characterCompare compFunc) {
-    	if (pos >= json.length())
+    	if (positionInvalid())
     		return false;
-        boolean comp = compFunc.checkChar(currentChar());
-        pos++;
-        return comp;
+        if (compFunc.checkChar(currentChar())) {
+        	pos++;
+        	return true;
+        }
+        return false;
     }
 
    
--- test/problem1/JSONParserTest.java
+++ test/problem1/JSONParserTest.java
@@ -1,6 +1,7 @@
 package test.problem1;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
 import problem1.*;
 
 
@@ -15,24 +16,157 @@ public class JSONParserTest {
     public static String emptyObjectWithWhiteSpace = "{ }";
     public static String emptyArrayWithWhiteSpace = "[   ]";
 
-    //simple tests
+//    simple tests
     @Test
-    public void testEmpty(){
+    public void testEmptyObject(){
         Map<String, Object> emptyMap = new HashMap<String, Object>();
         JSONParser emptyObjectParser = new JSONParser(JSONParserTest.emptyObject);
-        JSONParser emptyArrayParser = new JSONParser(JSONParserTest.emptyObject);
-        JSONParser emptyObjectWithWhiteSpaceParser = new JSONParser(JSONParserTest.emptyObject);
-        JSONParser emptyArrayWithWhiteSpaceParser = new JSONParser(JSONParserTest.emptyObject);
+        JSONParser emptyObjectWithWhiteSpaceParser = new JSONParser(JSONParserTest.emptyObjectWithWhiteSpace);
         try {
-		    assertEquals(emptyMap, emptyObjectParser.parse());
-		    assertEquals(emptyMap, emptyArrayParser.parse());
-		    assertEquals(emptyMap, emptyObjectWithWhiteSpaceParser.parse());
-		    assertEquals(emptyMap, emptyArrayWithWhiteSpaceParser.parse());
+		    assertEquals(emptyMap, emptyObjectParser.execute());
+		    assertEquals(emptyMap, emptyObjectWithWhiteSpaceParser.execute());
         } catch (Exception e){
         	e.printStackTrace();
+        	fail();
         }
         
     }
+    @Test
+    public void testEmptyArray(){
+    	Map<String, Object> emptyList = new HashMap<String, Object>();
+    	emptyList.put("", new ArrayList<Object>());
+        JSONParser emptyArrayParser = new JSONParser(JSONParserTest.emptyArray);
+        JSONParser emptyArrayWithWhiteSpaceParser = new JSONParser(JSONParserTest.emptyArrayWithWhiteSpace);
+        try {
+        	assertTrue(emptyArrayParser.execute().keySet().contains(""));
+        	assertTrue(emptyArrayWithWhiteSpaceParser.execute().keySet().contains(""));
+        	
+	        assertEquals(emptyArrayParser.execute().keySet().size(), 1);
+	        assertEquals(emptyArrayWithWhiteSpaceParser.execute().keySet().size(), 1);
+	    } catch (Exception e){
+	    	e.printStackTrace();
+	    	fail();
+	    	
+	    }
+    }
+    
+    @Test
+    public void testSimpleObject() {
+    	JSONParser parser = new JSONParser("{\"number\":123}");
+    	try {
+    		Map<String, Object> output = parser.execute();
+    		assertEquals(((JSONParser.ParsedNumber)output.get("number")).intComp,"123");
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    		fail();
+    	}
+    	
+    }
+    
+    @Test
+    public void testSimpleArray() {
+    	JSONParser parser = new JSONParser("[\"number\",123, null, true, false]");
+    	try {
+    		Map<String, Object> output = parser.execute();
+    		ArrayList<Object> items = (ArrayList<Object>) output.get("");
+    		assertEquals((String)items.get(0), "number");
+    		assertEquals(((JSONParser.ParsedNumber) items.get(1)).intComp,"123");
+    		assertEquals(items.get(2),null);
+    		assertEquals(items.get(3),true);
+    		assertEquals(items.get(4),false);
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    		fail();
+    	}
+    	
+    }
+    
+    @Test
+    public void testNumberParser() {
+    	JSONParser parser = new JSONParser("-1023.1342e2544");
+    	assertTrue(parser.parseNumber());
+    	parser = new JSONParser("0.1342e2544");
+    	assertTrue(parser.parseNumber());
+    	parser = new JSONParser("0.25333333334");
+    	assertTrue(parser.parseNumber());
+    	parser = new JSONParser("1342e2544");
+    	assertTrue(parser.parseNumber());
+    	parser = new JSONParser("2544");
+    	assertTrue(parser.parseNumber());
+    	parser = new JSONParser("-2544");
+    	assertTrue(parser.parseNumber());
+    	
+    	parser = new JSONParser("");
+    	assertFalse(parser.parseNumber());
+    	parser = new JSONParser("-");
+    	assertFalse(parser.parseNumber());
+    	parser = new JSONParser(".123");
+    	assertFalse(parser.parseNumber());
+    	parser = new JSONParser("0123");
+    	assertFalse(parser.parseNumber());
+    }
+
+    @Test
+    public void testStringParser() {
+    	JSONParser str = new JSONParser("\"teststring\"");
+    	assertTrue(str.parseString());
+    	str = new JSONParser("\"tests\"tring");
+    	assertTrue(str.parseString());
+    	str = new JSONParser("\"\"   ");
+    	assertTrue(str.parseString());
+    	
+    	
+    	JSONParser notString = new JSONParser("\"teststring");
+    	assertFalse(notString.parseString());
+    	notString = new JSONParser("teststring\"");
+    	assertFalse(notString.parseString());
+    	notString = new JSONParser("testst");
+    	assertFalse(notString.parseString());
+    	notString = new JSONParser("");
+    	assertFalse(notString.parseString());
+    }
+    @Test
+    public void testWhitespaceParser() {
+    	JSONParser parser = new JSONParser("  h  \r i  \n \t  j");
+    	parser.parseWhitespace();
+    	assertTrue(parser.checkChar('h'));
+    	parser.parseWhitespace();
+    	assertTrue(parser.checkChar('i'));
+    	parser.parseWhitespace();
+    	assertTrue(parser.checkChar('j'));
+    }
+    
+    @Test
+    public void testparseLiteral() {
+    	JSONParser emptyParser = new JSONParser("");
+    	assertFalse(emptyParser.parseLiteral("literal"));
+    	JSONParser nonemptyParser = new JSONParser("literal");
+    	assertTrue(nonemptyParser.parseLiteral("literal"));
+    	JSONParser longerParser = new JSONParser("literally");
+    	assertTrue(longerParser.parseLiteral("lit"));
+    }
+    
+    @Test
+	public void testCurrentChar() {
+		JSONParser whiteSpaceParser = new JSONParser("cs");
+		assertEquals(whiteSpaceParser.currentChar(), 'c');
+		whiteSpaceParser.checkChar('c');
+		assertEquals(whiteSpaceParser.currentChar(), 's');
+	}
+	
+	@Test
+	public void testCheckChar() {
+		JSONParser emptyParser = new JSONParser("");
+		JSONParser whiteSpaceParser = new JSONParser(" ");
+		assertFalse(emptyParser.checkChar('h'));
+		assertFalse(whiteSpaceParser.checkChar('h'));
+		  
+		JSONParser csParser = new JSONParser("cs");
+		assertTrue(csParser.checkChar('c'));
+		assertTrue(csParser.checkChar('s'));
+	  }
+	
+	
 }
 
 
